cac986532c58a94074f8d21deaad411a40904da4
Kebafilwe Joseph Loiti <josephloiti@gmail.com>
2022-01-18 10:10:33 +0200

    Add and Style the toggle button

---
+++ js/animate.js
@@ -0,0 +1,22 @@
+//Create Toggle button
+
+let btn = document.createElement("button");
+var btnDiv = document.getElementById("toggle-button");
+btn.innerHTML = "Toggle";
+btn.name = "toggle-button";
+btn.id = "togBtn";
+btnDiv.appendChild(btn);
+
+
+//onClick for Toggle Button
+var element = document.getElementById("togBtn");
+element.onclick = function(event) {
+    var toglBtn = document.getElementById("toggle-text");
+  if (toglBtn.innerHTML === "ACA just taught me about continuous integration and continuous deployment."
+  ) {
+    toglBtn.innerHTML = " ";
+  } else {
+    toglBtn.innerHTML = "ACA just taught me about continuous integration and continuous deployment.";
+    ;
+  }
+}
